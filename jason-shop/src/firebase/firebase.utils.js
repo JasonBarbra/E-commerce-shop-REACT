027d68ab954f041b887f4f2992c011d9c7cc868b
Jason <adaglo524@gmail.com>
2021-05-09 13:12:40 +0200

    implemented firebase utils sign in and sign up components

--- jason-shop/src/firebase/firebase.utils.js
+++ jason-shop/src/firebase/firebase.utils.js
@@ -36,7 +36,6 @@ const config = {
   }
   // Initialize Firebase
   firebase.initializeApp(config);
-
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
